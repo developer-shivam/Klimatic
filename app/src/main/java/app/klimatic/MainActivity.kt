f79661f19745a5ffe5dfae6ab0872f86f240daba
Shivam Satija <ssatija@mafcarrefour.com>
2021-06-06 17:21:13 +0530

    Initial commit

---
+++ app/src/main/java/app/klimatic/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.klimatic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
